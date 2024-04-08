526992e6c008d2f1bf19c928542ecb15f5bb26ff
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2024-04-08 11:52:26 -0700

    - Generate pseudo-random input in parallel to reduce benchmark runtime. - Add command line options to control number of elements, number of tiles, and   whether validation is performed.

--- inclusive_scan_decoupled_lookback.cpp
+++ inclusive_scan_decoupled_lookback.cpp
@@ -107,28 +107,54 @@ void inclusive_scan(stdr::range auto&& in, std::uint32_t num_tiles) {
     });
 }
 
-int main() {
-  constexpr auto N = 1024 * 1024 * 1024;
+int main(int argc, char** argv) {
+  std::uint32_t num_elements = 1024 * 1024 * 1024;
+  std::uint32_t num_tiles = 1024;
+  bool validate = true;
 
-  std::mt19937_64 gen(1337);
-  std::uniform_int_distribution<std::uint32_t> dis(0, 100);
+  if (argc > 1)
+    num_elements = std::stoul(argv[1]);
+  if (argc > 2)
+    num_tiles = std::stoul(argv[2]);
+  if (argc > 3)
+    validate = std::string_view("true") == std::string_view(argv[3]);
 
-  std::vector<std::uint32_t> in(N);
-  stdr::generate(in, [&] { return dis(gen); });
+  std::cout << "num_elements, " << num_elements << "\n";
+  std::cout << "num_tiles, " << num_tiles << "\n";
+  std::cout << "validate, " << validate << "\n";
 
-  std::vector<std::uint32_t> gold(N);
-  std::inclusive_scan(begin(in), end(in), begin(gold));
+  std::vector<std::uint32_t> in(num_elements);
+
+  auto all_tiles = stdv::iota(0U, num_tiles);
+  std::for_each(stde::par, begin(all_tiles), end(all_tiles),
+    [&] (std::uint32_t tile) {
+      auto sub_in = range_for_tile(in, tile, num_tiles);
+
+      std::minstd_rand gen(tile);
+      std::uniform_int_distribution<std::uint32_t> dis(0, 100);
+
+      stdr::generate(sub_in, [&] { return dis(gen); });
+    });
+
+  std::vector<std::uint32_t> gold;
+
+  if (validate) {
+    gold.resize(num_elements);
+    std::inclusive_scan(begin(in), end(in), begin(gold));
+  }
 
   auto start = std::chrono::high_resolution_clock::now();
 
-  inclusive_scan(in, 1024);
+  inclusive_scan(in, num_tiles);
 
-  auto end = std::chrono::high_resolution_clock::now();
+  auto finish = std::chrono::high_resolution_clock::now();
 
-  if (!stdr::equal(in, gold))
-    throw bool{};
-
-  std::chrono::duration<double> diff = end - start;
+  std::chrono::duration<double> diff = finish - start;
   std::cout << diff.count() << " [s]\n";
+
+  if (validate) {
+    if (!stdr::equal(in, gold))
+      throw bool{};
+  }
 }
 
